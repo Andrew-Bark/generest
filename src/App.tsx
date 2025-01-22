019a95fce41ff5c4c8f4ec3d350969bacc4c446c
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-22 10:42:47 +0000

    Added: Abstracted the initialising of the created modules (createShapes)

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,13 @@
 import './App.css';
 import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Vector3 } from 'three';
 import Shape from './components/Shape.tsx';
 import Line from './components/Line.tsx';
-import { Instrument, transport } from './instrument.tsx';
-import { Datasource } from './datasource.tsx';
 import Module from './models/module.ts';
 import { ConnectionObj, ModuleObj } from './models/types.ts';
 import Connection from './models/connection.ts';
 import Environment from './components/Environment.tsx';
+import { createShapes } from './helpers/shapes.helper.ts';
 import {
   addModuleUtility,
   updateModuleUtility
@@ -36,35 +34,10 @@ function App() {
     undefined
   );
 
-  // "Menu items" (i.e. one static instance of each module that can be cloned
-  //  and activated by dragging it into the interface area)
-  function createShapes() {
-    const datasource = new Module(
-      'datasource',
-      new Vector3(-3, 6, 0),
-      undefined,
-      new Datasource()
-    );
-
-    const instrument = new Module(
-      'instrument',
-      new Vector3(3, 6, 0),
-      new Instrument(),
-      undefined
-    );
-    addModule(datasource);
-    // addModule(trigger);
-    addModule(instrument);
-    // todo: do this dynamically when interacting with the datasource controls
-    const yesterday = new Date(Date.now());
-    yesterday.setDate(yesterday.getDate() - 1);
-    datasource.datasource?.getFullDay(yesterday);
-  }
-
   // create the "menu" shapes (ensure that this only runs once)
   // TODO: initialize with useEffect - outside of this function (move to top of file)
   if (modules.length === 0) {
-    createShapes();
+    createShapes(addModule);
     console.log('createShapes called');
   }
 
---
+++ src/helpers/shapes.helper.ts
@@ -0,0 +1,28 @@
+import Module from '../models/module';
+import { Vector3 } from 'three';
+import { Datasource } from '../datasource';
+import { Instrument } from '../instrument';
+
+export function createShapes(addModule: (module: Module) => void) {
+  const datasource = new Module(
+    'datasource',
+    new Vector3(-3, 6, 0),
+    undefined,
+    new Datasource()
+  );
+
+  const instrument = new Module(
+    'instrument',
+    new Vector3(3, 6, 0),
+    new Instrument(),
+    undefined
+  );
+
+  addModule(datasource);
+  // addModule(trigger);
+  addModule(instrument);
+
+  const yesterday = new Date(Date.now());
+  yesterday.setDate(yesterday.getDate() - 1);
+  datasource.datasource?.getFullDay(yesterday);
+}
